d4795f713ee15553834089fadb57ac442def4264
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-08 12:08:30 -0600

    create function to return a string for formatted student data

--- lib/data_processing.py
+++ lib/data_processing.py
@@ -1,4 +1,5 @@
 # This module contains functions to process student data.
+from lib.student_data import students
 
 def format_student_data(student):
     """
@@ -9,7 +10,7 @@ def format_student_data(student):
     - Major
     such as: "ID: 10 | Name: Louis Medina | Major: Computer Science"
     """
-    pass
+    return f"ID: {student.id} | Name: {student.name} | Major: {student.major}"
 
 def display_students(student_list):
     """
